7bee6b59843661f22fe61c8d02bd1c4cd6d56971
tiffany-tychewicz <tychewicz.design.studio@gmail.com>
2021-10-03 14:44:48 -0500

    i thought i was being clever with hiding my api key but it was a shit method. api key displays on frontend console

--- backend/server/index.js
+++ backend/server/index.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => { 
-  res.json({ apiKey: `${process.env.API_KEY_RAWG}` });
+app.get("/api/games", (req, res) => { 
+  
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
--- frontend/src/components/GameList.jsx
+++ frontend/src/components/GameList.jsx
@@ -1,67 +1,8 @@
-
-
-// class GameList extends React.Component {
-
-//   async getApi() {
-//     const [data, setData] = React.useState(null);
-
-//     React.useEffect(() => {
-//       fetch("/api")
-//         .then((res) => res.json())
-//         .then((data) => setData(data.apiKey));
-//     }, []);
-    
-//     this.getApi = data;
-//   }
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       games: [],
-//     };
-
-//     this.getGames = this.getGames.bind(this);
-//   }
-
-//   async getGames() {
-    
-//     const response = await fetch(`https://api.rawg.io/api/games?key=${this.getApi}`);
-//     const jsonData = await response.json();
-//     return jsonData.results;
-//   }
-
-//   async componentDidMount() {
-//     const games = await this.getGames();
-//     this.setState({ games });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {this.state.games.map((game) => (
-//           <p>{game.name}</p>
-//         ))}
-//       </div>
-//     );
-//   }
-// }
-
 import React, { useState, useEffect } from 'react';
 
-function GameList() {
-  const [gameList, setGameList] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch(import.meta.env.SNOWPACK_PUBLIC_API)
-      .then((res) => res.json())
-      .then((gameList) => setGameList(gameList.results));
-  }, []);  
-
-  console.log(gameList)
-
-  return (<p>test</p>);
+const GameList = () => {
   
+  return <div></div>;
 }
 
 
